60ec390e8618211d0e78fff9d920bc9e1866f8c1
Votre Nom <Vous@exemple.com>
2023-04-26 12:34:11 +0200

    Saé 102

---
+++ sae_s01.02_2022_2023-master/community_detection.py
@@ -0,0 +1,222 @@
+##############
+# SAE S01.01 #
+##############
+
+def liste_amis(amis, prenom):
+    """
+        Retourne la liste des amis de prenom en fonction du tableau amis.
+    """
+    prenoms_amis = []
+    i = 0
+    while i < len(amis)//2:
+        if amis[2 * i] == prenom:
+            prenoms_amis.append(amis[2*i+1])
+        elif amis[2*i+1] == prenom:
+            prenoms_amis.append(amis[2*i])
+        i += 1
+    return prenoms_amis
+
+def nb_amis(amis, prenom):
+    """ Retourne le nombre d'amis de prenom en fonction du tableau amis. """
+    return len(liste_amis(amis, prenom))
+
+
+def personnes_reseau(amis):
+    """ Retourne un tableau contenant la liste des personnes du réseau."""
+    people = []
+    i = 0
+    while i < len(amis):
+        if amis[i] not in people:
+            people.append(amis[i])
+        i += 1
+    return people
+
+def taille_reseau(amis):
+    """ Retourne le nombre de personnes du réseau."""
+    return len(personnes_reseau(amis))
+
+def lecture_reseau(path):
+    """ Retourne le tableau d'amis en fonction des informations contenues dans le fichier path."""
+    f = open(path, "r", encoding="utf-8") #Je me suis d'ajouter le paramètre "encoding"
+    l = f.readlines()
+    f.close()
+    amis = []
+    i = 0
+    while i < len(l):
+        fr = l[i].split(";")
+        amis.append(fr[0].strip())
+        amis.append(fr[1].strip())
+        i += 1
+    return amis
+
+def dico_reseau(amis):
+    """ Retourne le dictionnaire correspondant au réseau."""
+    dico = {}
+    people = personnes_reseau(amis)
+    i = 0
+    while i < len(people):
+        dico[people[i]] = liste_amis(amis, people[i])
+        i += 1
+    return dico
+
+def nb_amis_plus_pop(dico_reseau):
+    """ Retourne le nombre d'amis des personnes ayant le plus d'amis."""
+    personnes = list(dico_reseau)
+    maxi = len(dico_reseau[personnes[0]])
+    i = 1
+    while i < len(personnes):
+        if maxi < len(dico_reseau[personnes[i]]):
+            maxi = len(dico_reseau[personnes[i]])
+        i += 1
+    return maxi
+
+
+def les_plus_pop(dico_reseau):
+    """ Retourne les personnes les plus populaires, c'est-à-dire ayant le plus d'amis."""
+    max_amis = nb_amis_plus_pop(dico_reseau)
+    most_pop = []
+    personnes = list(dico_reseau)
+    i = 1
+    while i < len(personnes):
+        if len(dico_reseau[personnes[i]]) == max_amis:
+            most_pop.append(personnes[i])
+        i += 1
+    return most_pop
+
+##############
+# SAE S01.02 #
+##############
+
+def create_network(list_of_friends):
+    """
+        Retourne le dictionnaire correspondant au réseau
+    """
+    dico = {}
+    i = 0
+    while i < len(list_of_friends) // 2:
+        if list_of_friends[2*i] not in dico:
+            dico[list_of_friends[2*i]] = [list_of_friends[2*i+1]]
+        else:
+            dico[list_of_friends[2*i]] += [list_of_friends[2*i+1]]
+        
+        if list_of_friends[2*i+1] not in dico:
+            dico[list_of_friends[2*i+1]] = [list_of_friends[2*i]]
+        else:
+            dico[list_of_friends[2*i+1]] += [list_of_friends[2*i]]
+        i += 1
+    return dico
+
+    
+def get_people(network):
+    """
+        Retourne la liste des personnes du réseau dans un tableau.
+    """
+    
+    return list(network)
+
+def are_friends(network, person1, person2):
+    """
+        Retourne True si les deux personnes sont amies, et False sinon.
+    """
+    return person2 in network[person1]
+
+def all_his_friends(network, person, group):
+    """
+        Retourne True si la personne est amie avec toutes les personnes du groupe, et False sinon.
+    """
+    i = 0
+    while i < len(group):
+        if not are_friends(network, person, group[i]):
+            return False
+        i += 1
+    return True
+
+def is_a_community(network, group):
+    """
+        Retourne True si ce groupe est une communauté, et False sinon.
+    """
+    i = 0
+    while i < len(group):
+        tmp = group.copy()
+        person = tmp.pop(i)
+        if not all_his_friends(network, person, tmp):
+            return False
+        i += 1
+    return True
+
+def find_community(network, group):
+    """
+        Retourne une communauté en fonction de l'heuristique suivant :
+            - On part d'une communauté vide
+            - On considère les personnes les unes après les autres. Pour chacune des personnes,
+              si celle-ci est amie avec tous les membres de la communauté déjà créée,
+              alors on l'ajoute à la communauté.
+    """
+    community = []
+    i = 0
+    while i < len(group):
+        if all_his_friends(network, group[i], community):
+            community.append(group[i])
+        i += 1
+    return community
+
+def order_by_decreasing_popularity(network, group):
+    """
+        Trie le groupe de personnes selon la popularité(nombre d'amis) décroissante.
+    """
+    i = 0
+    while i < len(group):
+        max_val = len(network[group[i]])
+        maxi = i
+        j = i+1
+        while j < len(group):
+            if len(network[group[j]]) > max_val:
+                max_val = len(network[group[j]])
+                maxi = j
+            j += 1
+        group[i], group[maxi] = group[maxi], group[i]
+        i += 1
+    return group
+
+def find_community_by_decreasing_popularity(network):
+    """
+        Trie l'ensemble des personnes du réseau selon l'ordre décroissant de popularité
+        puis retourne la communauté trouvée en appliquant l'heuristique décrite dans la fonction find_community()
+    """
+    group = order_by_decreasing_popularity(network, get_people(network))
+    return find_community(network, group)
+
+def find_community_from_person(network, person):
+    """
+    Retourne une communauté maximale contenant cette personne selon l'heuristique suivante :
+        - On choisit une personne du réseau
+        - On crée une communauté contenant juste cette personne
+        - on considère les amis de cette personne par ordre de popularité décroissante. Pour chacune de ces personnes,
+          si celle-ci est amie avec tous les membres de la communauté déjà créée, 
+          alors on l'ajoute à la communauté.
+    """
+    community = [person]
+    group = order_by_decreasing_popularity(network, get_people(network))
+    i = 0
+    while i < len(group):
+        if all_his_friends(network, group[i], community) and group[i] != person:
+            community.append(group[i])
+        i += 1
+    return community
+
+def find_max_community(network):
+    """
+        Retourne la plus grande communauté trouvée en appliquant
+        l'heuristique de recherche de communauté maximale donnée
+        par find_community_from_person pour toutes les personnes du réseau
+    """
+    peoples = get_people(network)
+    max_val = len(find_community_from_person(network, peoples[0]))
+    maxi = 0
+    i = 1
+    while i < len(peoples):
+        if max_val < len(find_community_from_person(network, peoples[i])):
+            max_val = len(find_community_from_person(network, peoples[i]))
+            maxi = i
+        i += 1
+    return find_community_from_person(network, peoples[maxi])
---
+++ sae_s01.02_2022_2023-master/test_community_detection.py
@@ -0,0 +1,68 @@
+from community_detection import *
+
+amis_comm1 = lecture_reseau("files/Communaute1.csv")
+amis_comm2 = lecture_reseau("files/Communaute2.csv")
+dico_res_comm1 = create_network(amis_comm1)
+dico_res_comm2 = create_network(amis_comm2)
+
+def test_create_network():
+    assert dico_res_comm1 == dico_reseau(amis_comm1)
+    assert dico_res_comm2 == dico_reseau(amis_comm2)
+    print("Test 1 passed !")
+
+def test_get_people():
+    assert get_people(dico_res_comm1) == personnes_reseau(amis_comm1)
+    assert get_people(dico_res_comm2) == personnes_reseau(amis_comm2)
+    print("Test 2 passed !")
+
+def test_are_friends():
+    assert are_friends(dico_res_comm1, "Barbra", "Cloe")
+    assert are_friends(dico_res_comm2, "Olavi", "Vibol")
+    assert not are_friends(dico_res_comm1, "Mady", "Illtyd")
+    assert not are_friends(dico_res_comm2, "Marwa", "Lalie")
+    print("Test 3 passed !")
+
+def test_all_his_friends():
+    assert all_his_friends(dico_res_comm1, "Mady", ["Idelle", "Rufino", "Vittore", "Jakob", "Giedrius", "Barbra"])
+    assert all_his_friends(dico_res_comm2, "Ilies", ["Séverin", "Petru", "Christ", "Placide"])
+    assert not all_his_friends(dico_res_comm1, "Vittore", ["Cloe", "Rufino", "Olavi", "Björn", "Mady"])
+    assert not all_his_friends(dico_res_comm1, "Kirsa", ["Cloe", "Rufino", "Olavi", "Björn", "Mady"])
+    print("Test 4 passed !")
+
+def test_is_a_community():
+    assert is_a_community(dico_res_comm1, ["Mady", "Rufino", "Vittorio", "Barbra"])
+    assert is_a_community(dico_res_comm2, ["Dragan", "Cain", "Petru"])
+    assert not is_a_community(dico_res_comm1, ["Cloe", "Idelle", "Mady", "Björn", "Marwa"])
+    assert not is_a_community(dico_res_comm2, ["Illtyd", "Finn", "Placide", "Giedrius", "Stéphanie"])
+    print("Test 5 passed !")
+
+def test_find_community():
+    assert find_community(dico_res_comm1, ["Idelle", "Mady", "Idelle", "Teodor"]) == ["Idelle", "Mady"]
+    assert find_community(dico_res_comm1, ["Mady", "Rufino", "Olavi"]) == ["Mady", "Rufino"]
+    assert find_community(dico_res_comm2, ["Shayla", "Évike", "Simo", "Idelle"]) == ["Shayla"]
+    assert find_community(dico_res_comm2, ["Silviu", "Chatzkel", "Shirine", "Gordon"]) == ["Silviu"]
+    print("Test 6 passed !")
+
+def test_order_by_decreasing_popularity():
+    assert order_by_decreasing_popularity(dico_res_comm1, ["Mady"]) == ["Mady"]
+    assert order_by_decreasing_popularity(dico_res_comm1, ["Giedrius", "Mady", "Kirsa", "Vittore"]) == ["Mady", "Giedrius", "Vittore", "Kirsa"]
+    assert order_by_decreasing_popularity(dico_res_comm2, ["Zayneb"]) == ["Zayneb"]
+    assert order_by_decreasing_popularity(dico_res_comm2, ["Grwn", "Amadeo", "Cloe", "Pavao"]) == ["Pavao", "Amadeo", "Grwn", "Cloe"]
+    print("Test 7 passed !")
+
+def test_find_community_by_decreasing_popularity():
+    assert find_community_by_decreasing_popularity(dico_res_comm1) == ["Rufino", "Mady", "Vittorio", "Barbra"]
+    assert find_community_by_decreasing_popularity(dico_res_comm2) == ["Dragan", "Cain", "Petru"]
+    print("Test 8 passed !")
+
+def test_find_community_from_person():
+    assert find_community_from_person(dico_res_comm1, "Idelle") == ['Idelle', 'Mady', 'Cloe', 'Björn']
+    assert find_community_from_person(dico_res_comm1, "Giedrius") == ["Giedrius", "Rufino", "Mady", "Vittorio"]
+    assert find_community_from_person(dico_res_comm2, "Zayneb") == ['Zayneb', 'Glenys', 'Iagan']
+    assert find_community_from_person(dico_res_comm2, "Mady") == ['Mady', 'Séléna', 'Cain']
+    print("Test 9 passed !")
+
+def test_find_max_community():
+    assert find_max_community(dico_res_comm1) == ['Cloe', 'Mady', 'Vittorio', 'Barbra', 'Vittore']
+    assert find_max_community(dico_res_comm2) == ['Iagan', 'Dragan', 'Christ', 'Barbra']
+    print("Test 10 passed !")
